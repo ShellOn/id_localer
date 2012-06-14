cf08ed8489d565a4e05b8a341bee2dacb7b6a87b
ShellChen <chengjhong@gmail.com>
2012-06-15 05:39:10 +0800

    fix_done

--- lib/id_localer.rb
+++ lib/id_localer.rb
@@ -1,4 +1,7 @@
 require "i18n"
+require 'action_dispatch/http/request'
+require "rack/request"
+require "pry"
 
 #---- InfoDinamika Libraries ----
 # id_localer v.0.0.1
@@ -13,28 +16,37 @@ require "i18n"
 
 module IdLocaler
   class Localer
+     ACTION_DISPATCH_COOKIES = 'action_dispatch.cookies'.freeze
+     PATH_INFO = 'PATH_INFO'.freeze
+     HTTP_ACCEPT_LANGUAGE = 'HTTP_ACCEPT_LANGUAGE'.freeze
+
     def initialize(app)
       @app = app
+      @i18n_locales = I18n.available_locales.collect { |l| l.to_s}
     end
 
     def call(env)
-      s,h,b = @app.call(env) # call the before middlewares for get the status, header, body and the new env
-      params = env['action_dispatch.request.parameters']
-      cookies = env['action_dispatch.cookies']
       locale = nil
+      param_locale = nil
+      path_info = env[PATH_INFO] 
+      cookies = env[ACTION_DISPATCH_COOKIES]
+      http_accept_language = env[HTTP_ACCEPT_LANGUAGE]
+      #binding.pry
 
-      if params && locale = params[:locale] # put the routes into :locale scope in the config/routes.rb
-        #Switch to a determine language
-        if I18n.available_locales.include?(params[:locale])
-          locale = params[:locale]
-        else
-          locale = I18n.default_locale
-        end
+
+      if path_info && path_info != "/"
+        puts path_info+"path_info session------------------"
+        path_info_locale = (path_info.split("/") & @i18n_locales).first
+        param_locale = path_info_locale if @i18n_locales.include?(path_info_locale)
+      end
+      if param_locale
+        locale = param_locale
         cookies.permanent[:locale] = locale # write locale into the cookies
-      elsif cookies && cookies[:locale]
-        #Cookies local checking
+      elsif cookies && cookies[:locale] # TODO may do a @i18n_locales.include? check
+        puts "Cookies #{cookies[:locale]}"
         locale = cookies[:locale]
-      elsif locales = env['HTTP_ACCEPT_LANGUAGE']
+      elsif locales = http_accept_language
+        puts http_accept_language+"wwwwwwwwwwww"
         # detect the header
         locales = locales.split(/\s*,\s*/).collect do |l|
           l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
@@ -45,22 +57,19 @@ module IdLocaler
         end.collect do |l|
           l.first.downcase.gsub(/-[a-z0-9]+$/i) { |x| x.upcase }
         end
-        # Set the available languages in the i18n initialize file
-        # Ex:
-        # in config/initializers/locale.rb
-        # 
-        # I18n.available_locales = %w{ en zh es jp }
-        #
         locale = locales.map do |x| #en-US
-          I18n.available_locales.find do |y| # en
+          @i18n_locales.find do |y| # en
             y = y.to_s
             x == y || x.split('-', 2).first == y.split('-', 2).first
           end
         end.compact.first
         cookies.permanent[:locale] = locale # write locale into the cookies
+      else
+        locale = I18n.default_locale
+        cookies.permanent[:locale] = locale # write locale into the cookies
       end
-      locale = I18n.default_locale unless locale
-      I18n.locale = locale.to_s
+      I18n.locale = locale
+      s,h,b = @app.call(env)
       h['Content-Language'] = locale.to_s
       [s,h,b]
     end
